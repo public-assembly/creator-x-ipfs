35c8d6f8ce9f851f38c06dd84cac90eb69eff587
Salief Lewis <salieflewis@gmail.com>
2022-12-08 12:45:40 -0500

    add: nft storage config

---
+++ src/components/Metadata.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { StoreNFT } from './StoreNFT';
+
+export const Metadata = () => {
+  const [name, setName] = useState<string>();
+  const [description, setDescription] = useState<string>();
+  const [filename, setFilename] = useState<string>('');
+  const [fileToUpload, setFileToUpload] = useState<any>();
+
+  const handleInput = (event: any): void => {
+    setFilename(event?.target.files[0].name);
+    setFileToUpload(event?.target.files[0]);
+    console.log('File to upload:', fileToUpload);
+  };
+
+//   console.log('Name:', name);
+//   console.log('Description', description);
+
+  return (
+    <div className=''>
+      <div className='max-w-sm'>
+        <form className='flex flex-col gap-y-3 py-4'>
+          <label htmlFor='name'>Enter a name for your NFT</label>
+          <input
+            id='name'
+            name='name'
+            placeholder='Name'
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+            required
+          ></input>
+          <label htmlFor='name'>Provide a description of your NFT</label>
+          <input
+            id='description'
+            name='description'
+            placeholder='Description'
+            type='text'
+            value={description}
+            onChange={(e) => setDescription(e.currentTarget.value)}
+            required
+          ></input>
+          <div className='max-w-4rem py-2'>
+            <label htmlFor='image' className='btn-secondary'>
+              Select File
+            </label>
+          </div>
+          <input
+            id='image'
+            name='image'
+            type='file'
+            onChange={handleInput}
+            hidden
+            accept='image/jpg, image/jpeg, image/png, image/svg+xml, image/gif, video/mp4, video/quicktime, audio/mpeg, audio/mp3, audio/vnd.wav, audio/vnd.wave, audio/wav, audio/wave, audio/x-wav, audio/aiff'
+          ></input>
+          <div className=''>{filename}</div>
+        </form>
+      </div>
+      <StoreNFT
+        name={name}
+        description={description}
+        thumbnail={fileToUpload}
+      />
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { Account } from './Account';
 export { Create } from './Create';
 export { Header } from './Header';
+export { Metadata } from './Metadata';
 export { Storage } from './Storage';
+export { StoreNFT } from './StoreNFT';
